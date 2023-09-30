cb3b7d8f23d15a11f33aada266cbb962f549488b
Sébastien Pons <08.alesage-probleme@icloud.com>
2023-09-30 12:38:58 -0400

    Merge branch 'makeitglobal'

--- PluginSettings.cs
+++ PluginSettings.cs
@@ -2,20 +2,8 @@ using Newtonsoft.Json;
 
 public class PluginSettings
 {
-    public PluginSettingsFeature Feature => string.IsNullOrWhiteSpace(this.RawFeature) ? PluginSettingsFeature.Todos : Enum.Parse<PluginSettingsFeature>(this.RawFeature);
-
     [JsonProperty(PropertyName = "token")] public string Token { get; set; } = string.Empty;
 
     [JsonProperty(PropertyName = "serverUrl")]
     public string ServerUrl { get; set; } = string.Empty;
-
-
-    [JsonProperty(PropertyName = "rawfeature")]
-    public string RawFeature { get; set; } = string.Empty;
-}
-
-public enum PluginSettingsFeature
-{
-    Todos = 0,
-    MergeRequests = 1,
 }
--- TodosCounter/TodosCounter.cs
+++ TodosCounter/TodosCounter.cs
@@ -1,58 +1,61 @@
-﻿using BarRaider.SdTools;
+﻿using System.Diagnostics;
+using BarRaider.SdTools;
 
-[PluginActionId("dev.spons.gitlab.toggle")]
-public class ToggleAction : KeypadBase
+[PluginActionId("dev.spons.gitlab.todoscounter")]
+public class TodosCounter : KeypadBase
 {
     private const uint InactiveState = 0;
     private const uint ActiveState = 1;
-    private readonly PluginSettings _settings;
+    private readonly TodosCounterPluginSettings _settings;
     private readonly GitlabClient _gitlabClient;
 
-    // SDK does not allow us to set the tick interval, this is a workaround to only poke the API every 10 ticks (seconds)
-    private int _tickCount = 2;
+    private Logger _logger;
 
-    public ToggleAction(ISDConnection connection, InitialPayload payload)
+    // SDK does not allow us to set the tick interval, this is a workaround to only poke the API every 10 ticks (seconds)
+    private int _tickCount = 10;
+
+    public TodosCounter(ISDConnection connection, InitialPayload payload)
         : base(connection, payload)
     {
         Connection.SetTitleAsync("Loading...").Wait();
 
-        _settings = new PluginSettings();
+        _logger = new Logger(BarRaider.SdTools.Logger.Instance);
+
+        _settings = new TodosCounterPluginSettings();
         _gitlabClient = new GitlabClient();
 
         Tools.AutoPopulateSettings(_settings, payload.Settings);
 
         this.UpdateClient().Wait();
+        this.UpdateTodosCount().Wait();
     }
 
     public override void Dispose()
     {
     }
 
-    public override void KeyPressed(KeyPayload payload)
+    public override async void KeyPressed(KeyPayload payload)
     {
+        return;
     }
 
     public override async void KeyReleased(KeyPayload payload)
     {
+        Process.Start(new ProcessStartInfo(_settings.ServerUrl + "/dashboard/todos") { UseShellExecute = true });
+        await this.UpdateTodosCount();
     }
 
     public override async void OnTick()
     {
-        if (_tickCount < 2)
+        if (_tickCount < 10)
         {
             _tickCount++;
             return;
         }
 
-        if (_settings.Feature == PluginSettingsFeature.Todos)
-        {
-            await this.UpdateTodosCount();
-        }
-        else
-        {
-            await Connection.ShowAlert();
-        }
+        await this.UpdateTodosCount();
 
+        _tickCount = 0;
         _tickCount++;
     }
 
@@ -83,6 +86,15 @@ public class ToggleAction : KeypadBase
         {
             await Connection.ShowAlert();
             return;
+        }
+
+        if (todosCount > 0)
+        {
+            await Connection.SetStateAsync(ActiveState);
+        }
+        else
+        {
+            await Connection.SetStateAsync(InactiveState);
         }
 
         await Connection.SetTitleAsync(todosCount.ToString());
